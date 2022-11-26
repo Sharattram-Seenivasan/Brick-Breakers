18164878c759c2b5a6b2c73716ae84f7c3da48c6
Sharattram-Seenivasan <49329786+Sharattram-Seenivasan@users.noreply.github.com>
2022-11-26 18:57:52 -0500

    added save data

---
+++ scripts/save_file.gd
@@ -0,0 +1,33 @@
+extends Node
+
+const SAVE_FILE = "user://save_file.save"
+var game_data = {}
+
+
+func _ready():
+	load_data()
+
+
+func save_data():
+	var file = File.new()
+	file.open(SAVE_FILE, File.WRITE)
+	file.store_var(game_data)
+	file.close()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func load_data():
+	var file = File.new()
+	if not file.file_exists(SAVE_FILE):
+		game_data = {
+			"sound_effects_volume": 1.0,
+			"music_volume": 1.0,
+			"ball_speed": 2,
+			"paddle_size": 2,
+			"colorblind_mode": 0
+		}
+		save_data()
+	file.open(SAVE_FILE, File.READ)
+	game_data = file.get_var()
+	file.close()
